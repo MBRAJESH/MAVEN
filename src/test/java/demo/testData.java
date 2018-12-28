28b24e0bd5b8c687f472b200e717e26e1fa1d520
Rajesh MB <Rajesh MB@rajesh>
2018-12-28 09:09:43 +0530

    FIRST COMMIT

---
+++ src/test/java/demo/testData.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testData 
+{
+	@Test
+	public void testclas()
+	{
+		Reporter.log("hello rajesh",true);
+	}
+
+}
